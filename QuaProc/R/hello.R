e0503582458ab51ffee32c109767ab3b090a52f3
mol529 <mol529@users.noreply.github.com>
2021-02-11 16:29:57 +0800

    Update hello.R
    optimizing the parallel program

--- QuaProc/R/hello.R
+++ QuaProc/R/hello.R
@@ -389,247 +389,214 @@ sfStop()
 
 
 #######################--------------------------
-#RC_split
+#RC_bray
 #--------------------------------------------
-raup_crick_modified_split=function(spXsite,startn,endn, plot_names_in_col1=FALSE, classic_metric=FALSE, split_ties=TRUE, reps=9999, nocore=2, set_all_species_equal=FALSE, as.distance.matrix=TRUE, report_similarity=FALSE){
 
-  ##expects a species by site matrix for spXsite, with row names for plots, or optionally plots named in column 1.  By default calculates a modification of the Raup-Crick metric (standardizing the metric to range from -1 to 1 instead of 0 to 1). Specifying classic_metric=TRUE instead calculates the original Raup-Crick metric that ranges from 0 to 1. The option split_ties (defaults to TRUE) adds half of the number of null observations that are equal to the observed number of shared species to the calculation- this is highly recommended.  The argument report_similarity defaults to FALSE so the function reports a dissimilarity (which is appropriate as a measure of beta diversity).  Setting report_similarity=TRUE returns a measure of similarity, as Raup and Crick originally specified.  If ties are split (as we recommend) the dissimilarity (default) and similarity (set report_similarity=TRUE) calculations can be flipped by multiplying by -1 (for our modification, which ranges from -1 to 1) or by subtracting the metric from 1 (for the classic metric which ranges from 0 to 1). If ties are not split (and there are ties between the observed and expected shared number of species) this conversion will not work. The argument reps specifies the number of randomizations (a minimum of 999 is recommended- default is 9999).  set_all_species_equal weights all species equally in the null model instead of weighting species by frequency of occupancy.
-
-
+raup_crick_modified=function(spXsite,
+                             nocore=2,
+                             reps=9999,
+                             filename="all_sample"){
+  
+  ##spXsite: a species by site matrix, with row names for plots.  
   ##Note that the choice of how many plots (rows) to include has a real impact on the metric, as species and their occurrence frequencies across the set of plots is used to determine gamma and the frequency with which each species is drawn from the null model
-
-
-  ##this section moves plot names in column 1 (if specified as being present) into the row names of the matrix and drops the column of names
   library(vegan)
-  library(parallel)
-
-  if(plot_names_in_col1){
-    row.names(spXsite)<-spXsite[,1]
-    spXsite<-spXsite[,-1]
-  }
-
+  library(tidyr)
+  library(snowfall)
+  
   #对物种求和
-  com_sum <- apply(spXsite,2,sum)
-
+  com_sum <- colSums(spXsite)
+  
   ## count number of sites and total species richness across all plots，提取物种名称 (gamma)
   n_sites<-nrow(spXsite)
   n_sp <- ncol(spXsite)
   gamma <- colnames(spXsite)
-
-
+  
   ##make the spXsite matrix into a pres/abs. (overwrites initial spXsite matrix):
-  ceiling(spXsite/max(spXsite))->spXsite_standard
-
+  spXsite_standard <- decostand(spXsite,"pa")
   ##create an occurrence vector- used to give more weight to widely distributed species in the null model:
-  occur<-apply(spXsite_standard, MARGIN=2, FUN=sum)
-
-
-  ##NOT recommended- this is a non-trivial change to the metric:
-  ##sets all species to occur with equal frequency in the null model
-  ##e.g.- discards any occupancy frequency information
-  if(set_all_species_equal){
-    occur<-rep(1,n_sp)
-  }
-
-
-
-
-
-  #构建并行函数
-  func_subRC <- function(n,spXsite,alpha_levels,occur,a1,a2){
-    library(parallel)
-    library(vegan)
+  occur <- colSums(spXsite_standard)
+  
+  ## determine how many unique species richness values are in the dataset
+  ##this is used to limit the number of null communities that have to be calculated
+  alpha_levels<-sort(unique(rowSums(spXsite_standard)))
+  
+  #generate the matrix for the input of func_subRC function
+  mtr <- as.data.frame(matrix(0,length(alpha_levels),length(alpha_levels)))
+  rownames(mtr) <- colnames(mtr) <- 1:length(alpha_levels)
+  mtr[upper.tri(mtr)] <- NA
+  mtr$id2 <- rownames(mtr)
+  long_bray <- gather(mtr,key = "id1", value = "repsno",na.rm = T,-id2)
+  
+  a1 <- rep(long_bray$id1,reps)
+  a2 <- rep(long_bray$id2,reps)
+  repsno <- rep(1:reps, each = nrow(long_bray))
+  vfsmmy <- data.frame(a1,a2,repsno)
+  
+  #构建用于并行函数
+  #vf: containing 3 factors needed for cycle
+  func_subRC <- function(nl,vfsmmy,spXsite,alpha_levels,occur){
+    #library(snowfall)
+    #library(vegan)	 
+    a1 <- vfsmmy[nl,1]
+    a2 <- vfsmmy[nl,2]
+    n <- vfsmmy[nl,3]
+    
+    #finished_tmp <- nl
+    #write.table(finished_tmp,file = paste0(stt,"/",nl,"_finished.txt"),quote = F,row.names = F,col.names = F,sep = "\t")
+    
+    
     n_sp <- ncol(spXsite)
     gamma <- colnames(spXsite)
-    com_sum <- apply(spXsite,2,sum)
+    com_sum <- colSums(spXsite)
     ##two empty null communities of size gamma:
-    com1<-rep(0,n_sp)
-    com2<-rep(0,n_sp)
-    names(com1) <- gamma
-    names(com2) <- gamma
+    com1 <- com2 <- rep(0,n_sp)
+    names(com1) <- names(com2) <- gamma
+    
     ##add alpha1 number of species to com1, weighting by species occurrence frequencies:
     com1[sample(gamma, alpha_levels[a1], replace=FALSE, prob=occur)]<-1
-
     ##same for com2:
     com2[sample(gamma, alpha_levels[a2], replace=FALSE, prob=occur)]<-1
-
+    
     ##how many species are shared in common?
     #null_shared_spp[i]<-sum((com1+com2)>1)
-
+    
     ##有两种方法，测试证明，两者结果相似，mantel test结果R为99.1%，p为0.001.这里选择方法2。
     ###方法一：用所有样品平均多度作为概率抽取物种多度（会出现一部分物种多度为0）
-
+    
     #allreads1 <- sum(spXsite[a1,])
     #com1list <- which(com1==1)
     #com1_pro <- com_sum[com1list]
     #ind1 <- as.factor(sample(com1list,allreads1,replace=TRUE, prob=com1_pro))
     #ind1_summary <- t(as.matrix(table(ind1)))
-
+    
     #allreads2 <- sum(spXsite[a2,])
     #com2list <- which(com2==1)
     #com2_pro <- com_sum[com2list]
     #ind2 <- as.factor(sample(com2list,allreads2,replace=TRUE, prob=com2_pro))
     #ind2_summary <- t(as.matrix(table(ind2)))
-
-
+    
+    
     #pairma <- matrix(0,nrow=2,ncol=n_sp)
     #colnames(pairma) <- 1:n_sp
     #pairma[1,colnames(ind1_summary)] <- ind1_summary
     #pairma[2,colnames(ind2_summary)] <- ind2_summary
-
+    
     #dis1 <- vegdist(pairma,method="bray")
-
+    
     ###方法二：以所有样品平均多度为比例，计算物种多度（保证了所有样品多度不为0）
-    allreads1 <- sum(spXsite[a1,])
+    allreadcount <- sum(spXsite[1,])
+    
     com1list <- which(com1==1)
     com1_pro <- com_sum[com1list]
-    com1_pro <- allreads1*(com1_pro/sum(com1_pro))
-
-    allreads2 <- sum(spXsite[a2,])
+    com1_pro <- allreadcount*com1_pro/sum(com1_pro)
+    
     com2list <- which(com2==1)
     com2_pro <- com_sum[com2list]
-    com2_pro <- allreads2*(com2_pro/sum(com2_pro))
-
-
+    com2_pro <- allreadcount*com2_pro/sum(com2_pro)
+    
     pairma <- matrix(0,nrow=2,ncol=n_sp)
     colnames(pairma) <- gamma
     pairma[1,names(com1_pro)] <- com1_pro
     pairma[2,names(com2_pro)] <- com2_pro
-
+    
     dis2 <- vegdist(pairma,method="bray")
-
-
-    return(dis2)
+    
+    output <- c(alpha_levels[a1],alpha_levels[a2],n,dis2)
+    
+    return(output)
   }
-
-
-
-  ## determine how many unique species richness values are in the dataset
-  ##this is used to limit the number of null communities that have to be calculated
-  alpha_levels<-sort(unique(apply(spXsite_standard, MARGIN=1, FUN=sum)))
-
-  ##make_null:
-
-  ##alpha_table is used as a lookup to help identify which null distribution to use for the tests later.  It contains one row for each combination of alpha richness levels.
-
-  alpha_table<-data.frame(c(NA), c(NA))
-  names(alpha_table)<-c("smaller_alpha", "bigger_alpha")
-  col_count<-1
-
-  ##null_array will hold the actual null distribution values.  Each element of the array corresponds to a null distribution for each combination of alpha values.  The alpha_table is used to point to the correct null distribution- the row numbers of alpha_table correspond to the [[x]] indices of the null_array.  Later the function will find the row of alpha_table with the right combination of alpha values.  That row number is used to identify the element of null_array that contains the correct null distribution for that combination of alpha levels.
-  null_array<-list()
-
-  ##looping over each combination of alpha levels:
   
-  for(b1 in startn:endn){
-    for(b2 in b1:length(alpha_levels)){
-
-      cl <- makeCluster(nocore)
-      results <- parLapply(cl,1:reps,func_subRC,spXsite <- spXsite,alpha_levels<-alpha_levels,occur <- occur,a1 <- b1,a2 <- b2)
-      stopCluster(cl)
-      null_shared_spp <- as.matrix(rbind(results))
-
-
-      ##store null distribution, record values for alpha 1 and 2 in the alpha_table to help find the correct null distribution later:
-      null_array[[col_count]]<-null_shared_spp
-
-      alpha_table[col_count, which(names(alpha_table)=="smaller_alpha")]<-alpha_levels[b1]
-      alpha_table[col_count, which(names(alpha_table)=="bigger_alpha")]<-alpha_levels[b2]
-
-      #increment the counter for the columns of the alpha table/ elements of the null array
-      print(paste(b1,b2,sep = '_'))
-
-      col_count<-col_count+1
-
-    }
-  }
-
-  ##create a new column with both alpha levels to match on:
-  alpha_table$matching<-paste(alpha_table[,1], alpha_table[,2], sep="_")
-
-
-  #####################
-  ##do the test:
-
-
-
+  
+  #stt <- paste0("finished_",nrow(vfsmmy))
+  #if(file.exists(stt)){unlink(stt, recursive = T, force = T)}
+  #dir.create(stt)
+  
+  ##make_null:
+  sfInit(parallel = TRUE, cpus = nocore)
+  sfLibrary(vegan)
+  
+  sfExport("vfsmmy","spXsite", "alpha_levels", "occur")
+  results <- sfLapply(1:nrow(vfsmmy),func_subRC,vfsmmy <- vfsmmy, spXsite <- spXsite,alpha_levels<-alpha_levels,occur <- occur)
+  sfStop()
+  
+  null_shared_spp <- matrix(unlist(results), nrow=4)
+  null_shared_spp <- null_shared_spp[,order(null_shared_spp[2,])]
+  null_shared_spp <- null_shared_spp[,order(null_shared_spp[1,])]
+  null_shared_spp <- null_shared_spp[,order(null_shared_spp[3,])]
+  
+  print("Parallel processes finished")
+  
   ##build a site by site matrix for the results, with the names of the sites in the row and col names:
-  numrow <- endn - startn + 1
-  results<-matrix(data=NA, nrow=numrow, ncol=n_sites, dimnames=list(row.names(spXsite)[startn:endn], row.names(spXsite)))
-
-
+  lastresults<-matrix(data=NA, nrow=n_sites, ncol=n_sites, dimnames=list(row.names(spXsite), row.names(spXsite)))
+  
   ##for each pair of sites (duplicates effort now to make a full matrix instead of a half one- but this part should be minimal time as compared to the null model building)
-  for(i in 1:numrow){
-    for(j in 1:n_sites){
-
-      ##how many species are shared between the two sites:
-      #n_shared_obs<-sum((spXsite[i,]+spXsite[j,])>1)
-      n_shared_obs <- vegdist(spXsite[c(i,j),],method="bray")
-
-      ## what was the observed richness of each site?
-      obs_a1<-sum(spXsite_standard[i,])
-      obs_a2<-sum(spXsite_standard[j,])
-
-      ##place these alphas into an object to match against alpha_table (sort so smaller alpha is first)
-      obs_a_pair<-sort(c(obs_a1, obs_a2))
-
-      ##match against the alpha table- row index identifies which element of the null array contains the correct null distribution for the observed combination of alpha values:
-      null_index<-which(alpha_table$matching==paste(obs_a_pair[1], obs_a_pair[2], sep="_"))
-
-      ##how many null observations is the observed value tied with?
-      num_exact_matching_in_null<-sum(null_array[[null_index]]==n_shared_obs)
-
-      ##how many null values are bigger than the observed value?
-      num_greater_in_null<-sum(null_array[[null_index]]>n_shared_obs)
-
-
-
-      rc<-(num_greater_in_null)/reps
-
-
-
-
-      if(split_ties){
-
-        rc<-((num_greater_in_null+(num_exact_matching_in_null)/2)/reps)
-      }
-
-
-
-      if(!classic_metric){
-
-        ##our modification of raup crick standardizes the metric to range from -1 to 1 instead of 0 to 1
-
-        rc<-(rc-.5)*2
-      }
-
-
-      ## at this point rc represents an index of dissimilarity- multiply by -1 to convert to a similarity as specified in the original 1979 Raup Crick paper
-      if(report_similarity & !classic_metric){
-        rc<- rc*-1
-      }
-
-      ## the switch to similarity is done differently if the original 0 to 1 range of the metric is used:
-      if(report_similarity & classic_metric){
-        rc<- 1-rc
-      }
-
-
-      ##store the metric in the results matrix:
-      results[i,j]<-round(rc, digits=2)
-
-
-    }
+  
+  #mtr2 <- as.data.frame(matrix(0,n_sites,n_sites))
+  #rownames(mtr2) <- colnames(mtr2) <- 1:n_sites
+  mtr2 <- as.data.frame(lastresults)
+  mtr2[,] <- 0
+  mtr2[upper.tri(mtr2)] <- NA
+  mtr2$id1 <- rownames(mtr2)
+  longmtr <- gather(mtr2,key = "id2", value = "rn",na.rm = T,-id1)
+  
+  func_mtr <- function(nlm, longmtr, reps,spXsite, spXsite_standard,null_shared_spp){
+    ##how many species are shared between the two sites:
+    n_shared_obs <- vegdist(spXsite[c(longmtr[nlm,1],longmtr[nlm,2]),],method="bray")
+    
+    ## what was the observed richness of each site?
+    obs_a1<-sum(spXsite_standard[longmtr[nlm,1],])
+    obs_a2<-sum(spXsite_standard[longmtr[nlm,2],])
+    
+    ##place these alphas into an object to match against alpha_table (sort so smaller alpha is first)
+    obs_a_pair<-sort(c(obs_a1, obs_a2))
+    
+    nullrc <- null_shared_spp[4,null_shared_spp[1,]==obs_a_pair[1] & null_shared_spp[2,]==obs_a_pair[2]]
+    
+    ##how many null observations is the observed value tied with?
+    num_exact_matching_in_null<-sum(nullrc==n_shared_obs)
+    
+    ##how many null values are bigger than the observed value?
+    num_greater_in_null<-sum(nullrc>n_shared_obs)
+    
+    rc <- ((num_greater_in_null+(num_exact_matching_in_null)/2)/reps)			
+    ##our modification of raup crick standardizes the metric to range from -1 to 1 instead of 0 to 1
+    rc <- (rc - 0.5)*2
+    ## at this point rc represents an index of dissimilarity- multiply by -1 to convert to a similarity as specified in the original 1979 Raup Crick paper
+    rc<- -1*rc
+    
+    return(c(longmtr[nlm,1],longmtr[nlm,2],round(rc, digits=3)))
   }
-
-
-  if(as.distance.matrix){
-    results<-as.dist(results)
-  }
-
-
-  return(results)
-
+  
+  sfInit(parallel = TRUE, cpus = nocore)
+  sfLibrary(vegan)
+  
+  sfExport("longmtr","spXsite","reps", "spXsite_standard", "null_shared_spp")
+  
+  rcres <- sfLapply(1:nrow(longmtr),func_mtr,
+                    longmtr <- longmtr, 
+                    reps <- reps,
+                    spXsite <- spXsite,
+                    spXsite_standard<-spXsite_standard,
+                    null_shared_spp <- null_shared_spp)
+  
+  sfStop()
+  
+  longRC <- as.data.frame(t(matrix(unlist(rcres), nrow=3)))
+  
+  longRCs <- longRC[longRC$V1 != longRC$V2,]
+  longRCs <- longRCs[,c(2,1,3)]
+  colnames(longRCs) <- colnames(longRC)
+  longRC <- rbind(longRC,longRCs)
+  
+  longRC[,1] <- as.character(longRC[,1])
+  longRC[,2] <- as.character(longRC[,2])
+  longRC[,3] <- as.character(longRC[,3])
+  
+  lastresults <- pivot_wider(longRC,names_from = V2,values_from = V3)
+  
+  #unlink(stt, recursive = T, force = T)
+  
+  write.table(lastresults,file = paste(filename,"RCbray.txt",sep = "_"),quote = F,row.names = F,col.names = T,sep = "\t")
 }
 
 
